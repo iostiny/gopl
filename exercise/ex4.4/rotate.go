1a357b36d85b1b8a32950b3e0718767b477274a2
iostiny <iostiny@yahoo.com>
2018-05-25 19:17:39 +0800

    commit ex4.3 of slice
    Change-Id: I4449d68c51907dcfc5208337f42438a160a7f51e

---
+++ exercise/ex4.4/rotate.go
@@ -0,0 +1,15 @@
+// ex4.5 deques a slice of strings
+package unique
+
+func unique(strs []string) []string {
+	w := 0	// index of last written string
+	for _, s := range strs {
+		if strs[w] == s {
+			continue
+		}
+		w++ 
+		strs[w] = s
+	}
+
+	return strs[:w+1]
+}
