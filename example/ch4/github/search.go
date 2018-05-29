03d553e1a4e110741fae66f52598e35f7eb5ab26
iostiny <iostiny@yahoo.com>
2018-05-29 10:53:16 +0800

    add JSON exmaple
    Change-Id: I95250678a54c5525fe9eef2a0aa28c0833659493

---
+++ example/ch4/github/search.go
@@ -0,0 +1,44 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// SearchIssues querries the Github issue tracker.
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	//!-
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	//!+
+
+	// req, err := http.NewRequest("GET", IssuesURL+"?q="+q, nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// req.Header.Set(
+	// 	"Accept", "application/vnd.github.v3.text-match+json")
+	// resp, err := http.DefaultClient.Do(req)
+
+	// We must close resp.Body on all execution paths.
+	// (Chapter 5 present 'defer', which makes this simpler.)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
---
+++ example/ch4/issue/main.go
@@ -0,0 +1,50 @@
+// Issues prints a table of Github issues matching the search terms.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/iostiny/gopl/example/ch4/github"
+)
+
+// !+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issue:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
+
+// !-
+/* Output
+0 issue:
+#23331  mspiegel proposal: encoding/json: export the offset method of th
+#11046     kurin encoding/json: Decoder internally buffers full input
+#19469 chengzhic runtime: temporary object is not garbage collected
+#12001 lukescott encoding/json: Marshaler/Unmarshaler not stream friendl
+#22369     Splik encoding/json: add the full path to the field in Unmars
+#16212 josharian encoding/json: do all reflect work before decoding
+#5901        rsc encoding/json: allow override type marshaling
+#22752  buyology proposal: encoding/json: add access to the underlying d
+#7872  extempora encoding/json: Encoder internally buffers full output
+#17609 nathanjsw encoding/json: ambiguous fields are marshalled
+#7213  davechene cmd/compile: escape analysis oddity
+#22816 ganelon13 encoding/json: include field name in unmarshal error me
+#14750 cyberphon encoding/json: parser ignores the case of member names
+#20528  jvshahid net/http: connection reuse does not work happily with n
+#21092  trotha01 encoding/json: unmarshal into slice reuses element data
+#20754       rsc encoding/xml: unmarshal only processes first XML elemen
+#24768 billziss- runtime: Fatal Error in heapBitsForObject during GC on
+#20206 markdryan encoding/base64: encoding is slow
+#25426 josharian cmd/compile: revisit statement boundaries CL peformance
+#15808 randall77 cmd/compile: loads/constants not lifted out of loop
+#19109  bradfitz proposal: cmd/go: make fuzzing a first class citizen, l
+#17244       adg proposal: decide policy for sub-repositories
+*/
