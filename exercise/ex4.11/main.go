2ffded86683129c32ad1a6100c4735065f275972
iostiny <iostiny@yahoo.com>
2018-05-30 12:19:17 +0800

    add comment
    Change-Id: I0ec649f03779958180014e768d29788463258fa8

--- exercise/ex4.11/main.go
+++ exercise/ex4.11/main.go
@@ -151,3 +151,41 @@ func main() {
 		open_(owner, repo, number)
 	}
 }
+
+//+!
+/*
+$ ./4.11 search read golang go 5901
+
+#5901    aholbreich The newest 2.0.0-alpha.53 Quick start tutorials requires Python to be installed first
+#5901      asmeurer solve error with expression with derivative
+#5901      smandell Font Smoothing flickers when auto-savings on OS X and Safari
+#5901         Zero3 Wrong amount of time remaining during navigation
+#5901   vorpal-buil Exceeded slow_query limit (156.2 > 5) in mysql:
+#5901       fzwaard OrientDB 2.1.14-SNAPSHOT on Ubuntu: Failed Server KeepAlive and  NoKeepAliveTests
+#5901   dwang201510 spacemacs/report-issue fails if I set any layer variables with periods in them
+#5901        kimchy Improved bloom filter hashing
+#5901       vkuznet Fix bug in AJAX done function
+#5901    hyperlogic Threshold based walking/leaning while in HMD.
+#5901      electrum Hide all non-annotation Jackson classes from plugins
+#5901       dhensby Cleaning up GridFieldAddExistingAutocompleter
+#5901      spadgett Use assets/config.local.js if present for development config
+#5901    coderanger Remove the easy_install resource
+#5901      aphearin Add note on pre-releasing in affiliated package release docs
+#5901          sbko [Python] Increase range of valid status codes
+#5901        angrox PUP-6164: Tech-debt: links.puppetlabs.com -> links.puppet.com
+#5901       bryevdv pass HTTP request args more explicitly to session
+#5901         pitaj Use Benchpress
+#5901     HebaruSan Update folder path
+#5901      lpinsivy Update lifecycle in documentation
+#5901   zackJKnight Added a missing step to get started on Mac.
+#5901      suez1224 [FLINK-9235][Security] Add integration tests for YARN kerberos integration.
+#5901   nomanzafarm create process error 2
+#5901   MarcinSzysz Query: Wrong data in included navigation when using Skip() method
+#5901      Macludde USER ISSUE: Drag & Drop Item Stuck
+#5901       hyarsan [Feature Request] Add option for opacity to the RetroArch window
+#5901      joydance [As3]flash.utils.Object will be converted "*" type when compile to as3
+#5901   koorellasur replace a closed issue
+#5901         RoyGF CHT-Super Issue-Module Community
+
+*/
+//-!
